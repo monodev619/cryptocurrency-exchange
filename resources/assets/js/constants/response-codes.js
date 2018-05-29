057aab12cdbbe4814425385bc056027fdac79aa5
monodev619 <monodev619@gmail.com>
2018-05-29 14:27:32 +0800

    #2018/05/29 i18n

--- resources/assets/js/constants/response-codes.js
+++ resources/assets/js/constants/response-codes.js
@@ -1,3 +1,5 @@
 
-export const SUCCESS = 0;
-export const PASSWORD_MISMATCH = 401;
+export const SUCCESS            = 0;
+export const PASSWORD_MISMATCH  = 401;
+export const INVALID_CREDENTIAL = 402;
+
--- resources/assets/js/pages/auth/login.vue
+++ resources/assets/js/pages/auth/login.vue
@@ -4,29 +4,30 @@
       <div class="card-body">
         <form class="form-horizontal form-material" @submit.prevent="login" @keydown="form.onKeydown($event)">
           <h3 class="box-title m-b-20">Log In</h3>
+          <div class="has-danger" v-if="invalid_credential"><div class="form-control-feedback">{{ $t('invalid_credential') }}</div></div>
           <div class="form-group ">
             <div class="col-xs-12">
-              <input class="form-control" type="text" required="" name="email" v-model="form.email" placeholder="Email"> </div>
+              <input class="form-control" type="text" required="" name="email" v-model="form.email" :placeholder="$t('email')"> </div>
           </div>
           <div class="form-group">
             <div class="col-xs-12">
-              <input class="form-control" type="password" required="" name="password" v-model="form.password" placeholder="Password"> </div>
+              <input class="form-control" type="password" required="" name="password" v-model="form.password" :placeholder="$t('password')"> </div>
           </div>
           <div class="form-group">
             <div class="col-md-12">
               <div class="checkbox checkbox-primary pull-left p-t-0">
                 <input id="checkbox-signup" type="checkbox" name="remember" v-model="remember">
-                <label for="checkbox-signup"> Remember me </label>
-              </div> <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+                <label for="checkbox-signup"> {{ $t('remember_me') }} </label>
+              </div> <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> {{ $t('forgot_password') }}</a> </div>
           </div>
           <div class="form-group text-center m-t-20">
             <div class="col-xs-12">
-              <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+              <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">{{ $t('login') }}</button>
             </div>
           </div>
           <div class="form-group m-b-0">
             <div class="col-sm-12 text-center">
-              <p>Don't have an account? <router-link :to="{name: 'register'}" class="text-info m-l-5"><b>Register</b></router-link></p>
+              <p>Don't have an account? <router-link :to="{name: 'register'}" class="text-info m-l-5"><b>{{ $t('register') }}</b></router-link></p>
             </div>
           </div>
         </form>
@@ -54,6 +55,7 @@
 
 <script>
 import Form from 'vform'
+import * as codes from '~/constants/response-codes';
 
 export default {
   middleware: 'guest',
@@ -63,17 +65,23 @@ export default {
   },
 
   data: () => ({
-    form: new Form({
-      email: '',
-      password: ''
-    }),
-    remember: false
+      form: new Form({
+        email: '',
+        password: ''
+      }),
+      remember: false,
+      invalid_credential: false
   }),
 
   methods: {
     async login () {
+        this.invalid_credential = false;
       // Submit the form.
       const { data } = await this.form.post('/_api/login');
+      
+      if ( data.code == codes.INVALID_CREDENTIAL ) {
+          this.invalid_credential = true;
+      }
 
       // Save the token.
       this.$store.dispatch('auth/saveToken', {
