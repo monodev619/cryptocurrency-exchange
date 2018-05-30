1756ca16a00ab9d7bc8bd4760f1ce0e91a871e57
monodev619 <monodev619@gmail.com>
2018-05-30 08:41:10 +0800

    #2018/05/29 add custom css

--- resources/assets/js/layouts/default.vue
+++ resources/assets/js/layouts/default.vue
@@ -1,23 +1,18 @@
 <template>
   <body class="fix-header">
-
-    <div id="main-wrapper">
-
-      <header class="topbar">
-        <navbar/>
-      </header>
-
-      <div class="height"></div>
-
-      <div class="content">
-
-        <!--<div class="preloader">-->
-          <!--<svg class="circular" viewBox="25 25 50 50">-->
-            <!--<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>-->
-        <!--</div>-->
-        <child/>
-
-      </div>
+        <div id="main-wrapper">
+              <header class="topbar">
+                <navbar/>
+              </header>
+              <div class="height"></div>
+              <div class="content">
+                <!--<div class="preloader">-->
+                  <!--<svg class="circular" viewBox="25 25 50 50">-->
+                    <!--<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>-->
+                <!--</div>-->
+                <child/>
+              </div>
+        <footer class="footer"> © 2018 Admin Press Admin by themedesigner.in </footer>
     </div>
   </body>
 </template>
--- resources/assets/js/pages/wallets/balance.vue
+++ resources/assets/js/pages/wallets/balance.vue
@@ -4,254 +4,41 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">BITCOIN MARKETS</h4>
-                        <h6 class="card-subtitle">Total Volume = 10480.670 &nbsp; 1 BTC = $7,410.786</h6>
-                        <div class="table-responsive m-t-40">
-                            <table id="myTable" class="table table-bordered table-striped">
+                        <h4 class="card-title m-t-10">Balances</h4>
+                        <!--<h6 class="card-subtitle">Total Volume = 10480.670 &nbsp; 1 BTC = $7,410.786</h6>-->
+                        <div class="table-responsive m-t-20">
+                            <table id="tblbalances" class="table table-bordered table-striped">
                                 <thead>
                                 <tr>
-                                    <th>MARKET</th>
                                     <th>CURRENCY</th>
-                                    <th>VOLUME</th>
-                                    <th>CHANGE</th>
-                                    <th>LAST PRICE</th>
-                                    <th>24HR HIGH</th>
-                                    <th>24HR LOW</th>
-                                    <th>%SPREAD</th>
-                                    <th>ADDED</th>
+                                    <th>SYMBOL</th>
+                                    <th>AVAIABLE BALANCE</th>
+                                    <th>PENDING DEPOSIT</th>
+                                    <th>TOTAL</th>
+                                    <th>EST.BTC VALUE</th>
+                                    <th>%CHANGE</th>
+                                    <th></th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td>Howard Hatfield</td>
-                                    <td>Office Manager</td>
-                                    <td>San Francisco</td>
-                                    <td>51</td>
-                                    <td>2008/12/16</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Hope Fuentes</td>
-                                    <td>Secretary</td>
-                                    <td>San Francisco</td>
-                                    <td>41</td>
-                                    <td>2010/02/12</td>
-                                    <td>$109,850</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
+                                    <td>Bitcoin</td>
+                                    <td>BTC</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>%0.8</td>
                                     <td>$164,500</td>
                                 </tr>
                                 <tr>
-                                    <td>Vivian Harrell</td>
-                                    <td>Financial Controller</td>
-                                    <td>San Francisco</td>
-                                    <td>62</td>
-                                    <td>2009/02/14</td>
-                                    <td>$452,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Timothy Mooney</td>
-                                    <td>Office Manager</td>
-                                    <td>London</td>
-                                    <td>37</td>
-                                    <td>2008/12/11</td>
-                                    <td>$136,200</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Jackson Bradshaw</td>
-                                    <td>Director</td>
-                                    <td>New York</td>
-                                    <td>65</td>
-                                    <td>2008/09/26</td>
-                                    <td>$645,750</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Olivia Liang</td>
-                                    <td>Support Engineer</td>
-                                    <td>Singapore</td>
-                                    <td>64</td>
-                                    <td>2011/02/03</td>
-                                    <td>$234,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Bruno Nash</td>
-                                    <td>Software Engineer</td>
-                                    <td>London</td>
-                                    <td>38</td>
-                                    <td>2011/05/03</td>
-                                    <td>$163,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Sakura Yamamoto</td>
-                                    <td>Support Engineer</td>
-                                    <td>Tokyo</td>
-                                    <td>37</td>
-                                    <td>2009/08/19</td>
-                                    <td>$139,575</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Thor Walton</td>
-                                    <td>Developer</td>
-                                    <td>New York</td>
-                                    <td>61</td>
-                                    <td>2013/08/11</td>
-                                    <td>$98,540</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Finn Camacho</td>
-                                    <td>Support Engineer</td>
-                                    <td>San Francisco</td>
-                                    <td>47</td>
-                                    <td>2009/07/07</td>
-                                    <td>$87,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Serge Baldwin</td>
-                                    <td>Data Coordinator</td>
-                                    <td>Singapore</td>
-                                    <td>64</td>
-                                    <td>2012/04/09</td>
-                                    <td>$138,575</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Zenaida Frank</td>
-                                    <td>Software Engineer</td>
-                                    <td>New York</td>
-                                    <td>63</td>
-                                    <td>2010/01/04</td>
-                                    <td>$125,250</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Zorita Serrano</td>
-                                    <td>Software Engineer</td>
-                                    <td>San Francisco</td>
-                                    <td>56</td>
-                                    <td>2012/06/01</td>
-                                    <td>$115,000</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Jennifer Acosta</td>
-                                    <td>Junior Javascript Developer</td>
-                                    <td>Edinburgh</td>
-                                    <td>43</td>
-                                    <td>2013/02/01</td>
-                                    <td>$75,650</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Cara Stevens</td>
-                                    <td>Sales Assistant</td>
-                                    <td>New York</td>
-                                    <td>46</td>
-                                    <td>2011/12/06</td>
-                                    <td>$145,600</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Hermione Butler</td>
-                                    <td>Regional Director</td>
-                                    <td>London</td>
-                                    <td>47</td>
-                                    <td>2011/03/21</td>
-                                    <td>$356,250</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Lael Greer</td>
-                                    <td>Systems Administrator</td>
-                                    <td>London</td>
-                                    <td>21</td>
-                                    <td>2009/02/27</td>
-                                    <td>$103,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Jonas Alexander</td>
-                                    <td>Developer</td>
-                                    <td>San Francisco</td>
-                                    <td>30</td>
-                                    <td>2010/07/14</td>
-                                    <td>$86,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Shad Decker</td>
-                                    <td>Regional Director</td>
-                                    <td>Edinburgh</td>
-                                    <td>51</td>
-                                    <td>2008/11/13</td>
-                                    <td>$183,000</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Michael Bruce</td>
-                                    <td>Javascript Developer</td>
-                                    <td>Singapore</td>
-                                    <td>29</td>
-                                    <td>2011/06/27</td>
-                                    <td>$183,000</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
-
-                                </tr>
-                                <tr>
-                                    <td>Donna Snider</td>
-                                    <td>Customer Support</td>
-                                    <td>New York</td>
-                                    <td>27</td>
-                                    <td>2011/01/25</td>
-                                    <td>$112,000</td>
-                                    <td>$164,500</td>
-                                    <td>$164,500</td>
+                                    <td>Bitcoin</td>
+                                    <td>BTC</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>0.00000000</td>
+                                    <td>%0.8</td>
                                     <td>$164,500</td>
                                 </tr>
                                 </tbody>
@@ -265,13 +52,13 @@
 </template>
 
 <script>
-     import '../../plugins/jquery.dataTables.min.js';
+     import '~/plugins/datatables/jquery.dataTables.min';
 
     export default {
         name: "balance",
         middleware: 'auth',
         mounted() {
-            $('#myTable').dataTable();
+            $('#tblbalances').dataTable();
         }
     }
 </script>
