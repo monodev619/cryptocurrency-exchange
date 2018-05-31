d8c0679c528e7f3e8db32e29f90313c888918e73
CG <worldc584@gmail.com>
2018-05-31 11:20:42 +0900

    5/31 add modal(deposit and withdraw)

--- resources/assets/js/layouts/default.vue
+++ resources/assets/js/layouts/default.vue
@@ -6,13 +6,9 @@
               </header>
               <div class="height"></div>
               <div class="content">
-                <!--<div class="preloader">-->
-                  <!--<svg class="circular" viewBox="25 25 50 50">-->
-                    <!--<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>-->
-                <!--</div>-->
                 <child/>
-                  <footer class="footer" v-if="show_footer"> © 2018 Admin Press Admin by themedesigner.in </footer>
               </div>
+            <footer class="footer" v-if="show_footer"> © 2018 Admin Press Admin by themedesigner.in </footer>
 
     </div>
   </body>
--- resources/assets/js/pages/wallets/balance.vue
+++ resources/assets/js/pages/wallets/balance.vue
@@ -5,42 +5,47 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title m-t-10">Balances</h4>
-                        <!--<h6 class="card-subtitle">Total Volume = 10480.670 &nbsp; 1 BTC = $7,410.786</h6>-->
                         <div class="table-responsive m-t-20">
                             <table id="tblbalances" class="table table-bordered table-striped">
                                 <thead>
-                                <tr>
-                                    <th>CURRENCY</th>
-                                    <th>SYMBOL</th>
-                                    <th>AVAIABLE BALANCE</th>
-                                    <th>PENDING DEPOSIT</th>
-                                    <th>TOTAL</th>
-                                    <th>EST.BTC VALUE</th>
-                                    <th>%CHANGE</th>
-                                    <th></th>
-                                </tr>
+                                    <tr>
+                                        <th>CURRENCY</th>
+                                        <th>SYMBOL</th>
+                                        <th>AVAIABLE BALANCE</th>
+                                        <th>PENDING DEPOSIT</th>
+                                        <th>TOTAL</th>
+                                        <th>EST.BTC VALUE</th>
+                                        <th>%CHANGE</th>
+                                        <th></th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
-                                    <td>Bitcoin</td>
-                                    <td>BTC</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>%0.8</td>
-                                    <td>$164,500</td>
-                                </tr>
-                                <tr>
-                                    <td>Bitcoin</td>
-                                    <td>BTC</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>0.00000000</td>
-                                    <td>%0.8</td>
-                                    <td>$164,500</td>
-                                </tr>
+                                    <tr>
+                                        <td>Bitcoin</td>
+                                        <td>BTC</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>%0.8</td>
+                                        <td>
+                                            <button class="btn btn-googleplus" type="button" data-toggle="modal" data-target="#depositModal"> <i class="mdi mdi-arrow-up-bold"></i> </button>
+                                            <button class="btn btn-twitter waves-effect waves-light" type="button" data-toggle="modal" data-target="#withdrawModal"> <i class="mdi mdi-arrow-down-bold"></i></button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Bitcoin</td>
+                                        <td>BTC</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>0.00000000</td>
+                                        <td>%0.8</td>
+                                        <td>
+                                            <button class="btn btn-googleplus" type="button" data-toggle="modal" data-target="#depositModal"> <i class="mdi mdi-arrow-up-bold"></i> </button>
+                                            <button class="btn btn-twitter waves-effect waves-light" type="button" data-toggle="modal" data-target="#withdrawModal"> <i class="mdi mdi-arrow-down-bold"></i></button>
+                                        </td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -48,19 +53,118 @@
                 </div>
             </div>
         </div>
+        <!--<modal></modal>-->
+            <div class="modal fade" id="depositModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Deposit</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="currency">
+                                    <div class="col-xs-6">Currency</div>
+                                </div>
+                                <div class="current-currency">
+                                    <div class="current-header row">
+                                        <div class="col-sm-6"><span class="label-name">NAME</span></div>
+                                        <div class="col-sm-6"><span class="symbol">SYMBOL</span> </div>
+                                    </div>
+                                    <div><hr></div>
+                                    <div class="current-body row">
+                                        <div class="col-sm-6"><span class="coinname">Bitcoin</span></div>
+                                        <div class="col-sm-6"><span class="symbolname">BTC</span></div>
+                                    </div>
+                                </div>
+                                <div class="disclaimer">
+                                    <div class="disc-title">DISCLAIMER</div>
+                                    <p class="disc-content">I acknowledge the following information:<br>
+                                        By depositing tokens to this address, you agree to our <a href="https://bittrex.zendesk.com/hc/en-us/articles/115000961172" target="_blank">deposit recovery policy</a>.
+                                        Depositing tokens to an address other than BTC may result in your funds being lost.
+                                    </p>
+                                </div>
+                                <div class="address">
+                                    <span class="addr-title">Address</span><br>
+                                    <input type="text" class="addr-input" id="addr" placeholder="Error generating address">
+                                    <button class="addr-button">New Address</button>
+                                </div>
+                                <div class="de-instruct">
+                                    <h2 class="title">DEPOSIT INSTRUCTIONS</h2>
+                                    <p class="instruct-content">Depositing tokens to this address other than BTC will result in your funds being lost.</p>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-twitter waves-effect waves-light" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-danger waves-effect waves-light">Done</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="withdrawModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Withdrawal</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="currency">
+                                    <div class="col-xs-6">Currency</div>
+                                </div>
+                                <div class="current-currency withdraw">
+                                    <div class="current-header row">
+                                        <div class="col-sm-4"><span class="label-name">NAME</span></div>
+                                        <div class="col-sm-4"><span class="symbol">SYMBOL</span> </div>
+                                        <div class="col-sm-4"><span class="balance">AVAILABLE BALANCE</span></div>
+                                    </div>
+                                    <div><hr></div>
+                                    <div class="current-body row">
+                                        <div class="col-sm-4"><span class="coinname">Bitcoin</span></div>
+                                        <div class="col-sm-4"><span class="symbolname">BTC</span></div>
+                                        <div class="col-sm-4"><span class="balancevalue">0</span></div>
+                                    </div>
+                                </div>
+                                <div class="withdrawamount row">
+                                    <h2 class="col-sm-12 withdraw-title">Withdrawal amount</h2>
+                                    <div class="col-sm-6">QUANTITY</div>
+                                    <div class="col-sm-6"><input type="text" class="quantityvalue" v-bind:value="0"><span class="cointype">BTC</span></div>
+                                    <div class="col-sm-6">TRANSACTION FEE</div>
+                                    <div class="col-sm-6"><span class="feevalue">0.00050000 &nbsp;BTC</span></div>
+                                    <div class="col-sm-6">TOTAL WITHDRAWAL</div>
+                                    <div class="col-sm-6"><span class="totalvalue">-0.00050000 &nbsp;BTC</span></div>
+                                </div>
+                                <div class="address withdraw">
+                                    <span class="addr-title">Address</span><br>
+                                    <input type="text" class="withdraw-addr-input" id="addr" placeholder="Error generating address">
+                                </div>
+                                <div class="de-instruct withdraw">
+                                    <h2 class="title">DISCLAIMER</h2>
+                                    <p class="instruct-content">Please verify your withdrawal address. We cannot refund an incorrect withdrawal.</p>
+                                    <p>DO NOT WITHDRAW DIRECTLY TO A CROWDFUND OR ICO.</p>
+                                    <p class="instruct-content">We will not credit your account with tokens from that sale.</p>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-twitter waves-effect waves-light" data-dismiss="modal">Cancel</button>
+                                <button type="button" class="btn btn-danger waves-effect waves-light">Withdraw BTC</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
     </div>
 </template>
 
 <script>
      import '~/plugins/datatables/jquery.dataTables.min';
+     import '~/plugins/sticky-kit.min.js';
 
-    export default {
-        name: "balance",
-        middleware: 'auth',
-        mounted() {
-            $('#tblbalances').dataTable();
-        }
-    }
+
 </script>
 
 <style scoped>
